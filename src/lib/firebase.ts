f2c8e455cfc990ce1970139e54ed62fd1758731e
Hridoy Chondro <work.hridoy.chondro@gmail.com>
2025-05-26 06:12:28 +0000

    // Import the functions you need from the SDKs you need import { initializeApp } from "firebase/app"; import { getAnalytics } from "firebase/analytics"; // TODO: Add SDKs for Firebase products that you want to use // https://firebase.google.com/docs/web/setup#available-libraries
    // Your web app's Firebase configuration
    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
    const firebaseConfig = {
      apiKey: "AIzaSyC_VtRLDWKCWHIZh_gqC5kEHg7qgHvJOxU",
      authDomain: "testproject-d47ea.firebaseapp.com",
      databaseURL: "https://testproject-d47ea-default-rtdb.asia-southeast1.firebasedatabase.app",
      projectId: "testproject-d47ea",
      storageBucket: "testproject-d47ea.firebasestorage.app",
      messagingSenderId: "842523620219",
      appId: "1:842523620219:web:eba03879bcb0c3921803de",
      measurementId: "G-SLZ3JWVZ4E"
    };
    
    // Initialize Firebase
    const app = initializeApp(firebaseConfig);
    const analytics = getAnalytics(app);

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,6 +9,8 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, // Added measurementId
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL, // Added databaseURL
 };
 
 let app: FirebaseApp;
